263c9d07c35b55ee09c3df6305ca16c17aaffbd0
Krupanszky.marcell <marci.krupanszky@gmail.com>
2024-05-16 08:46:10 +0200

    page object model

---
+++ Bejelentkezés/Bejelentkezés/utilities/Base.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bejelentkezés.utilities
+{
+    public class Base
+    {
+        public static jsonReader reader { get; set; } = new jsonReader();
+        public IWebDriver driver;
+        [SetUp]
+        public void Init()
+        {
+            driver = new ChromeDriver("chromedriver.exe");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            driver.Manage().Window.Maximize();
+            string? actUrl = ConfigurationManager.AppSettings["Url"];
+            driver.Url = actUrl;
+
+        }
+        [TearDown]
+        public void Cleanup()
+        {
+
+            driver.Dispose();
+        }
+    }
+}
---
+++ Bejelentkezés/Bejelentkezés/utilities/jsonReader.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bejelentkezés.utilities
+{
+    public class jsonReader
+    {
+        public List<string> extractdataList(string tokenname)
+        {
+            string myJsonString = File.ReadAllText("tests/tests.json");
+            var jsonObject = JToken.Parse(myJsonString);
+            return jsonObject.SelectToken(tokenname).Values<string>().ToList();
+        }
+    }
+}
