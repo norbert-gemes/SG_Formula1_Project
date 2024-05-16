0ca925022a4e7a966454f96aa0cdde73867f6336
Krupanszky.marcell <marci.krupanszky@gmail.com>
2024-05-16 08:26:43 +0200

    projeckt

---
+++ Bejelentkezés/Bejelentkezés/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Bejelentkezés
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
