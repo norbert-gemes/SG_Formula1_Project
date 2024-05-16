c790b3467103b83d11fc93645f6fccfc71f03d67
kaplon14 <gergely.kaplon@students.jedlik.eu>
2024-05-16 08:49:47 +0200

    szia

---
+++ TicketTest/TicketTest/pages/MainPage.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketTest.pages
+{
+    public class MainPage
+    {
+        IWebDriver driver;
+        public MainPage(IWebDriver webDriver)
+        {
+            driver = webDriver;
+            PageFactory.InitElements(driver, this);
+        }
+        [FindsBy(How = How.XPath, Using = "//*[@id=\"onetrust-accept-btn-handler\"]")]
+        public IWebElement popup { get; set; }
+
+    }
+}
---
+++ TicketTest/TicketTest/utilities/Base.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketTest.utilities
+{
+    public class Base
+    {
+        public static jsonReader reader { get; set; }  = new jsonReader();
+        public IWebDriver driver;
+        [SetUp]
+        public void Init()
+        {
+            driver = new ChromeDriver("chromedriver.exe");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.Manage().Window.Maximize();
+            string? actUrl = ConfigurationManager.AppSettings["Url"];
+            driver.Url = actUrl;
+        }
+        [TearDown]
+        public void Cleanup() 
+        { 
+            driver.Dispose();
+        }
+    }
+}
